4d4be47f29ccbd81706853a4b1547a6db758c33e
Luuk Rutten <luuk.rutten@cloudnation.nl>
2025-04-04 15:05:15 +0200

    dbug

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 from password import check_password
 import streamlit as st
-# from menu import homepage_menu
+from menu import homepage_menu
 
 
 if not check_password():
     st.stop()  # Do not continue if check_password is not True.
 
-# # Initialize st.session_state.role to None
-# if "role" not in st.session_state:
-#     st.session_state.role = None
+# Initialize st.session_state.role to None
+if "role" not in st.session_state:
+    st.session_state.role = None
 
-# # Retrieve the role from Session State to initialize the widget
-# st.session_state._role = st.session_state.role
+# Retrieve the role from Session State to initialize the widget
+st.session_state._role = st.session_state.role
 
-# homepage_menu() # Render the dynamic homepage menu!
+homepage_menu() # Render the dynamic homepage menu!
